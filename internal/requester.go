f7ad91a918f5f975fbed4dd225af42734b3f7e46
Tom Riley <thomriley@gmail.com>
2021-11-09 21:44:30 -0600

    comment cleanup, exit logging

--- internal/requester.go
+++ internal/requester.go
@@ -1,18 +1,19 @@
 package internal
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/spf13/viper"
 )
 
+//Requester interface needs an API path to request JSON data
 type Requester interface {
 	BuildPath() string
 }
 
-//requests the JSON data for all fields in a Clickup task or subtask
+//Gets JSON data for any struct that implements Requester interface
 func GetJSON(r Requester) []byte {
 	apiPath := r.BuildPath()
 	token := viper.GetString("ctoken")
@@ -23,11 +24,9 @@ func GetJSON(r Requester) []byte {
 	req.Header.Add("Authorization", token)
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
-
 	if err != nil {
-		fmt.Println("Errored when sending request to the server")
+		log.Fatalln("Errored when sending request to the server")
 	}
-
 	defer resp.Body.Close()
 	resp_body, _ := ioutil.ReadAll(resp.Body)
 
