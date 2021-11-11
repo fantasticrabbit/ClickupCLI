756ae55f8ffa61f4e6a34089d49ce870082dc9d1
Tom Riley <thomriley@gmail.com>
2021-11-11 12:29:38 -0600

    basic GetJSON test

--- internal/requester_test.go
+++ internal/requester_test.go
@@ -6,16 +6,15 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/fantasticrabbit/ClickupCLI/internal"
 	"github.com/fantasticrabbit/ClickupCLI/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
 func init() {
-	internal.Client = &mocks.MockClient{}
+	Client = &mocks.MockClient{}
 }
 
-func TestGetClientResponseSuccess(t *testing.T) {
+func TestGetJSON(t *testing.T) {
 	json := `{"name":"Test Name","full_name":"test full name","owner":{"login": "admin"}}`
 	mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
 		r := ioutil.NopCloser(bytes.NewReader([]byte(json)))
@@ -24,7 +23,7 @@ func TestGetClientResponseSuccess(t *testing.T) {
 			Body:       r,
 		}, nil
 	}
-	request := internal.getJSON("http://localhost/testpath")
+	request := getJSON("http://localhost/testpath")
 	assert.NotNil(t, request)
-	assert.EqualValues(t, "Test Name", request.Name)
+	assert.EqualValues(t, json, request)
 }
