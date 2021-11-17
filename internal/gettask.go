bf998ee9615ebead43268780df6157b2876633d0
Tom Riley <thomriley@gmail.com>
2021-11-16 22:20:48 -0600

    change getjson return type to match cleanup

--- internal/gettask.go
+++ internal/gettask.go
@@ -23,6 +23,6 @@ func (t TaskRequest) BuildPath() string {
 }
 
 //GetJSON accepts an API path and returns byte payload of JSON data
-func (t TaskRequest) GetJSON(apiPath string) string {
+func (t TaskRequest) GetJSON(apiPath string) []byte {
 	return getJSON(apiPath)
 }
